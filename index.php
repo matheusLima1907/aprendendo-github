8bc0d8a380a2f177b29583ab92d55761163df5fb
Matheus Alexandre <matheusalexandrerlima@gmail.com>
2021-02-09 17:26:52 -0300

    Pagina inicial

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+    <div id="header">
+        <h3>Minha Página Principal</h3>
+    </div>
+
+    <div id="content">
+        <h1>Conteúdo Principal</h1>
+    </div>
+
+    <div id="footer">
+    <h4>Rodapé</h4>
+    </div>
+
+<body>
+    
+</body>
+</html>
